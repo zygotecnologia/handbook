d3bb77fdd0d97a22cfb1b93e3c32adc68f1dee41
Lucas Prim <lucas@sumone.com.br>
2016-02-24 20:05:52 -0300

    Updated Handbook to reflect our reality

--- compile.js
+++ compile.js
@@ -1,7 +1,7 @@
 var markdownpdf = require("markdown-pdf");
 var fs = require("fs");
 
-fs.createReadStream("index.markdown")
+fs.createReadStream("README.md")
   .pipe(markdownpdf())
   .pipe(fs.createWriteStream("output/S1-Handbook.pdf"))
 
